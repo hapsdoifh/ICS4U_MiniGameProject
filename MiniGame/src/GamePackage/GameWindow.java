716b65f8848ef332a86640c69d1a1e5fad09fac8
linh-KAK <114237304+linh-KAK@users.noreply.github.com>
2022-11-21 09:48:43 -0500

    Add files via upload

--- MiniGame/src/GamePackage/GameWindow.java
+++ MiniGame/src/GamePackage/GameWindow.java
@@ -3,18 +3,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package GamePackage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import javax.swing.Timer;
 
 /**
  *
- * @author zuhan
+ * @author lTruUsr
  */
 public class GameWindow extends javax.swing.JFrame {
 
+    MenuPage mp;
+    TypingGame MainGame;
+    
+    Timer timer;
+    int second, minute;
+    String ddSecond, ddMinute;
+    
+    DecimalFormat dFormat = new DecimalFormat("00");
+    
     /**
      * Creates new form GameWindow
      */
-    public GameWindow() {
+    public GameWindow(MenuPage mp) {
         initComponents();
+        MainGame = new TypingGame("RandomWords.txt");     
+        if(!MainGame.getRoundState()){//round not started
+            MainGame.startRound(jTextArea1,jTextField3);    
+        }    
+        this.mp = mp;
+        
+        
+        jLabel5.setText("03:00");
+        second = 0;
+        minute = 3;
+
     }
 
     /**
@@ -27,104 +51,176 @@ public class GameWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel1.setPreferredSize(new java.awt.Dimension(736, 404));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Stencil", 0, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Type:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(29, 25, 110, 48));
+
+        jTextField3.setBackground(new java.awt.Color(253, 101, 101));
+        jTextField3.setFont(new java.awt.Font("SimSun", 0, 18)); // NOI18N
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });
+        jTextField3.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTextField3KeyReleased(evt);
+            }
+        });
+        jPanel1.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 202, 675, 93));
+
+        jLabel2.setFont(new java.awt.Font("Stencil", 0, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("SCORE:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(92, 313, 98, -1));
+
+        jLabel3.setFont(new java.awt.Font("Stencil", 0, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("WPM:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(331, 313, 71, -1));
+
+        jLabel4.setFont(new java.awt.Font("Stencil", 0, 24)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("timer:");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(567, 313, 111, -1));
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setBackground(new java.awt.Color(253, 101, 101));
         jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("SimSun", 0, 18)); // NOI18N
+        jTextArea1.setLineWrap(true);
         jTextArea1.setRows(5);
-        jTextArea1.setText("Instructions/words");
+        jTextArea1.setWrapStyleWord(true);
         jScrollPane1.setViewportView(jTextArea1);
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, 680, 100));
+
+        jButton1.setBackground(new java.awt.Color(253, 101, 101));
+        jButton1.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Next Level");
+        jButton1.setBorderPainted(false);
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
             }
         });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 370, 160, 20));
 
-        jButton1.setText("Pause");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButton2.setBackground(new java.awt.Color(253, 101, 101));
+        jButton2.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("Menu");
+        jButton2.setBorderPainted(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 30, 180, 30));
 
-        jButton2.setText("Exit");
-
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
-        jLabel1.setText("WPM/SCORE");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jScrollPane1)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGap(0, 152, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jTextField1))
-                        .addGap(151, 151, 151))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2)
-                        .addContainerGap())))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton1))
-                .addContainerGap())
-        );
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("jLabel5");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 340, 100, 20));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        setVisible(false);
+        mp.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        // TODO add your handling code here:        
+        if(!MainGame.getRoundState()){//round not started
+            MainGame.startRound(jTextArea1,jTextField3);            
+        }    
+        jTextField3.setText("");
+        
+    }//GEN-LAST:event_jButton1MouseClicked
 
+    private void jTextField3KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField3KeyReleased
+        // TODO add your handling code here:
+        if(!MainGame.getRoundState()){//round not started
+            //MainGame.startRound(jTextArea1,jTextField3);            
+        }else{
+            if(jTextField3.getText().contains(" ")){ //check this word
+                if(MainGame.CheckWord(jTextField3)){
+                    jLabel2.setText(""+MainGame.getScore());
+                }
+            }
+        }
+    }//GEN-LAST:event_jTextField3KeyReleased
+
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField3ActionPerformed
+
+     public void Timer() {
+        timer = new Timer(1000, new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                second--;
+             
+                ddSecond = dFormat.format(second);
+                ddMinute = dFormat.format(minute);
+                
+                jLabel5.setText(ddMinute + ":" + ddSecond);
+                
+                if (second==-1) {
+                    second=59;
+                    minute--;
+                    
+                    ddSecond = dFormat.format(second);
+                    ddMinute = dFormat.format(minute);
+                    jLabel5.setText(ddMinute + ":" + ddSecond);
+                   
+                }
+                
+                if (minute==0 && second==0) {
+                    timer.stop();
+                }
+            }
+        });
+        
+        timer.start();
+    }
+
+    
     /**
      * @param args the command line arguments
      */
@@ -155,7 +251,6 @@ public class GameWindow extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GameWindow().setVisible(true);
             }
         });
     }
@@ -164,9 +259,13 @@ public class GameWindow extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
--- MiniGame/src/GamePackage/Level.java
+++ MiniGame/src/GamePackage/Level.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package GamePackage;
 
-/**
- *
- * @author zuhan
- */
-public class Level {
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+import java.math.*;
+
+public class Level{
+    protected String[] text;
+    public int difficulty;
+    protected int checkIndex;
+    protected int time; //in seconds
+    protected int Mistakes;
+    protected ArrayList<String> Words = new ArrayList<String>();
     
+    public Level(String Filename){
+        difficulty = 1;
+        checkIndex = 0;
+        Mistakes = 0;
+        try{
+            File tempFile = new File("src\\GamePackage\\RandomWords.txt");
+            Scanner myScan = new Scanner(tempFile);
+            int i = 0;
+            while (myScan.hasNext()){
+                Words.add( myScan.nextLine());
+            }
+        }
+        catch(Exception e){}
+    }
+    public int getMistakes(){
+        return Mistakes;
+    }
+    
+    public void clear(){
+        checkIndex = 0;
+        time = 0; //in seconds
+        Mistakes = 0;
+        text = null;
+    }
+    public void DoLevel(javax.swing.JTextArea Dest, javax.swing.JTextField Source){
+        int rangeBot = Words.size()/20*(difficulty>=20?19:difficulty-1);
+        int rangeTop = Words.size()/20*(difficulty>=20?20:difficulty);
+        int L = (int)(10+difficulty*1.5);
+        text = new String[L];
+        String output = "";
+        for (int i = 0; i<L;i++){
+            text[i] = Words.get((int)(Math.random()*(rangeTop-rangeBot))+rangeBot);
+            output += text[i];
+            output += " ";
+        }
+        Dest.setText(output);
+    }
+    public boolean checkMistake(String In,javax.swing.JTextField Source){
+        int mistake = 0;
+        for(int i = 0; i<text[checkIndex].length();i++){
+            if(i>=In.length()){
+                mistake += text[checkIndex].length() - In.length();
+                break;
+            }
+            else if(text[checkIndex].charAt(i) != In.charAt(i)){
+                mistake++;
+            }
+        }
+        checkIndex++;
+        Mistakes += mistake;
+        Source.setText("");
+        if(checkIndex >= (int)(10+difficulty*1.5)){
+            difficulty ++;
+            return true;//round over
+        }else{
+            return false;
+        }
+    }
 }
